8d979efd9b9814b48e9acad8658b0b95cfdbf6ef
himanshu1145 <himanshu.yadav@market-xcel.com>
2024-11-20 17:29:10 +0530

    changes

--- XMC-FRONTEND/src/features/auth/register/PersonalDetailPage.jsx
+++ XMC-FRONTEND/src/features/auth/register/PersonalDetailPage.jsx
@@ -28,7 +28,6 @@ import CheckboxComponent from "../../../components/Checkbox/CheckboxComp";
 import { useSignUp } from "@clerk/clerk-react";
 import axios from "axios";
 
-
 const StyledPopper = styled(Popper)({
   border: "1px solid #e0e0e0",
   borderRadius: "8px",
@@ -88,7 +87,6 @@ const PersonalDetails = ({ setActiveStep }) => {
       profession: data.professions,
       privacyPolicy: data.termsAgreement,
     };
-    
 
     if (!isLoaded) return;
     setLoading(true);
