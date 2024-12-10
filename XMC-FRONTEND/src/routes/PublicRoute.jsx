fcd88b339e9fd0a06866861750c53a542a36166b
himan07 <himan9714@gmail.com>
2024-12-10 20:07:24 +0530

    profile page added

--- XMC-FRONTEND/src/routes/PublicRoute.jsx
+++ XMC-FRONTEND/src/routes/PublicRoute.jsx
@@ -1,44 +1,10 @@
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@clerk/clerk-react";
-import Cookies from 'js-cookie'; 
-
-
+import Cookies from "js-cookie";
 
 const PublicRoute = ({ children }) => {
-  const token = Cookies.get("__session")
   const { isLoaded, isSignedIn } = useAuth();
   const navigate = useNavigate();
-
-
-  const trackStep = parseInt(localStorage.getItem("activeStep"), 10);
-  const email = sessionStorage.getItem("userEmail") || "";
-
-  // useEffect(() => {
-  //   if (!isLoaded) return;
-  
-  //   if (token) {
-  //     navigate("/dashboard");
-  //     return;
-  //   }
-  
-  //   switch (trackStep) {
-  //     case 1:
-  //       navigate("/register/personal-details");
-  //       break;
-  //     case 1:
-  //       if (email) {
-  //         navigate("/register/verification");
-  //       }
-  //       break;
-  //     case 2:
-  //       navigate("/register/professional-details");
-  //       return;
-  //     default:
-  //       navigate("/");
-  //   }
-  // }, [isLoaded, token, trackStep, email, navigate]);
-  
 
   if (!isLoaded) {
     return null;
